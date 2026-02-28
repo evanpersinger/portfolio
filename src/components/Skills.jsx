c3e28c534654e21a87a2c2152ac752bb97aef6ef
Evan Daniel Persinger <evan9persinger@gmail.com>
2026-02-28 17:11:29 -0500

    profile pic enlarged, edited technologies

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,72 +1,38 @@
 import React from 'react'
-import { SiPython, SiR, SiPostgresql, SiJavascript } from 'react-icons/si'
+import { SiPython, SiR, SiPostgresql, SiJavascript, SiPandas, SiNumpy, SiScikitlearn, SiTensorflow, SiPytorch, SiJupyter, SiGit, SiDocker, SiAmazonaws, SiTableau, SiMicrosoftexcel, SiMysql, SiMongodb } from 'react-icons/si'
 import { FaDatabase } from 'react-icons/fa'
 import './Skills.css'
 
 function Skills() {
-  const skillCategories = [
-    {
-      title: 'Programming Languages',
-      skills: [
-        { name: 'Python', icon: SiPython },
-        { name: 'R', icon: SiR },
-        { name: 'SQL', icon: FaDatabase },
-        { name: 'JavaScript', icon: SiJavascript }
-      ]
-    },
-    {
-      title: 'Data Science & ML',
-      skills: [
-        { name: 'Pandas', icon: null },
-        { name: 'NumPy', icon: null },
-        { name: 'Scikit-learn', icon: null },
-        { name: 'TensorFlow', icon: null },
-        { name: 'PyTorch', icon: null },
-        { name: 'Matplotlib', icon: null },
-        { name: 'Seaborn', icon: null }
-      ]
-    },
-    {
-      title: 'Tools & Technologies',
-      skills: [
-        { name: 'Jupyter', icon: null },
-        { name: 'Git', icon: null },
-        { name: 'Docker', icon: null },
-        { name: 'AWS', icon: null },
-        { name: 'Tableau', icon: null },
-        { name: 'Excel', icon: null }
-      ]
-    },
-    {
-      title: 'Databases',
-      skills: [
-        { name: 'PostgreSQL', icon: SiPostgresql },
-        { name: 'MySQL', icon: null },
-        { name: 'MongoDB', icon: null }
-      ]
-    }
+  const allSkills = [
+    { name: 'Python', icon: SiPython },
+    { name: 'R', icon: SiR },
+    { name: 'SQL', icon: FaDatabase },
+    { name: 'JavaScript', icon: SiJavascript },
+    { name: 'PyTorch', icon: SiPytorch },
+    { name: 'Jupyter', icon: SiJupyter },
+    { name: 'Git', icon: SiGit },
+    { name: 'Docker', icon: SiDocker },
+    { name: 'AWS', icon: SiAmazonaws },
+    { name: 'Tableau', icon: SiTableau },
+    { name: 'PostgreSQL', icon: SiPostgresql },
+    { name: 'MySQL', icon: SiMysql },
+    { name: 'MongoDB', icon: SiMongodb }
   ]
 
   return (
     <section id="technologies" className="skills">
       <h2 className="section-title">Technologies</h2>
-      <div className="skills-grid">
-        {skillCategories.map((category, index) => (
-          <div key={index} className="skill-category">
-            <h3 className="category-title">{category.title}</h3>
-            <div className="skill-tags">
-              {category.skills.map((skill, skillIndex) => {
-                const IconComponent = skill.icon
-                return (
-                  <span key={skillIndex} className="skill-tag">
-                    {IconComponent && <IconComponent className="skill-icon" />}
-                    {skill.name}
-                  </span>
-                )
-              })}
-            </div>
-          </div>
-        ))}
+      <div className="skill-tags">
+        {allSkills.map((skill, index) => {
+          const IconComponent = skill.icon
+          return (
+            <span key={index} className="skill-tag">
+              {IconComponent && <IconComponent className="skill-icon" />}
+              {skill.name}
+            </span>
+          )
+        })}
       </div>
     </section>
   )
