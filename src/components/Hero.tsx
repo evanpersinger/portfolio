180e1d5d2b754fa3f9e7e948423420b846e822be
Evan Daniel Persinger <evan9persinger@gmail.com>
2026-06-28 21:07:59 -0700

    removed some icons, made icons bigger

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -17,8 +17,7 @@ function Hero() {
           <div className="hero-info">
             <p className="hero-item">Data Science Student at Western University<img src="/icons/uwo_icon.png" alt="Western University" className="company-icon" /></p>
             <p className="hero-item">Backend Engineering Intern at Biztrip AI<img src="/icons/biztrip_icon.png" alt="Biztrip AI" className="company-icon biztrip-icon" /></p>
-            <p className="hero-item">San Francisco, CA <img src="/icons/Niners.png" alt="49ers" className="location-flag" /> <img src="/icons/USA_Flag.png" alt="US flag" className="location-flag" /></p>
-            <p className="hero-item">London, ON <img src="/icons/Canadian_Flag.png" alt="Canadian flag" className="location-flag" /></p>
+            <p className="hero-item">San Francisco, CA <img src="/icons/USA_Flag.svg" alt="US flag" className="location-flag" /></p>
           </div>
           <div className="hero-links">
             <a href="/resume" className="hero-link">Resume</a>
